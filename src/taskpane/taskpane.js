7d0ff5a75f1193a146c52e6e32eb125277e93d72
isheep <sheepy.workshop@gmail.com>
2019-09-12 18:23:25 +0900

    primary complete!

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -16,27 +16,54 @@ Office.onReady(info => {
   if (info.host === Office.HostType.Word) {
     // document.getElementById("sideload-msg").style.display = "none";
     // document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-    document.getElementById("repFile").onchange = replaceFromFile;
-    document.getElementById("console").innerText = "onReady OK\n"
+    // document.getElementById("run").onclick = run;
+    document.getElementById("select_repfile").onchange = readFileByClick;
+    document.getElementById("drop_zone").ondragover = handleDragOver;
+    document.getElementById("drop_zone").ondragenter = handleDragEnter;
+    document.getElementById("drop_zone").ondrop = readFileByDrop;
+    document.getElementById("console").innerText = "（・ω・＠） \n"
+    // document.getElementById("use_clipboad").onclick = readFromClipboard;
+    document.getElementById("from_textarea").onclick = readFromTextarea;
   }
 });
 
 function wl(log) {
-  document.getElementById("console").innerText += log + "\n";
+  document.getElementById("console").innerText = log;
 }
 
-export async function replaceFromFile(ev) {
+function wlp(log) {
+  document.getElementById("console").innerText += "\n" + log;
+}
+
+function handleDragEnter(ev) {
+  ev.stopPropagation();
+  ev.preventDefault();
+}
+
+function handleDragOver(ev) {
+  ev.stopPropagation();
+  ev.preventDefault();
+}
+
+function readFileByDrop(ev) {
+  ev.stopPropagation();
+  ev.preventDefault();
+  readRepFile(ev.dataTransfer.files[0])
+}
+
+function readFileByClick(ev) {
+  ev.stopPropagation();
+  ev.preventDefault();
+  readRepFile(ev.target.files[0]);
+}
+
+export async function readRepFile(repFile) {
   const fr = new FileReader;
-  const repFile = ev.target.files[0];
   const myAnnotator = new RegExp("^[0-9_/\[\\\]]+$");
-  // wl(repFile.name);
   if (!repFile.name.endsWith("tsv") && !repFile.name.endsWith("csv")) {
-    wl(repFile.name)
-    wl("File extention incorrect")
+    wl("(x_x＠)MEEE! I only can eat a 'CSV' or 'TSV' File")
     return
   }
-  let counter = 0;
   fr.readAsText(repFile);
   fr.onload = () => {
     const repPairs = [];
@@ -44,29 +71,87 @@ export async function replaceFromFile(ev) {
     for (let line of lines) {
       if (line !== "") {
         const eachTerm = line.split("\t");
-        wl(eachTerm[0])
+        if (eachTerm[0] !== "" && eachTerm[1] !== "") {
+          if (!myAnnotator.test(eachTerm[0])) {
+            repPairs.push(line.split("\t"));
+          }
+        }
+      }
+    }
+    wl("(^_^＠) YUMMY!");
+    executeReplace(repPairs);
+  }
+}
+
+export async function readFromClipboard(ev) {
+  let cdata;
+  const myAnnotator = new RegExp("^[0-9_/\[\\\]]+$");
+  const repPairs = [];
+  if (window.clipboardData) {
+    cdata = window.clipboardData.getData("Text").replace(/\r?\n/g, "\n");
+  } else {
+    cdata = ev.clipboardData;
+  }
+  if (cdata == "") {
+    return;
+  }
+  const lines = cdata.split("\n");
+  for (let line of lines) {
+    if (line !== "") {
+      const eachTerm = line.split("\t");
+      if (eachTerm[0] !== "" && eachTerm[1] !== "") {
         if (!myAnnotator.test(eachTerm[0])) {
           repPairs.push(line.split("\t"));
         }
       }
     }
-    return Word.run(async context => {
-      document.getElementById("console").innerText += "Now In word.run\n"
-      for (let repPair of repPairs) {
-        counter++
-        // let searchResults = context.document.body.search(repPair[0], { matchCase: vc.matchCase, useWildcard: vc.useWildcard });
-        let searchResults = context.document.body.search(repPair[0]);
-        searchResults.load(["text", "font"]);
-        await context.sync()
-        for (var i = 0; i < searchResults.items.length; i++) {
-          searchResults.items[i].insertText("[_" + String(counter) + "_]" + repPair[1] + "[_/_]", "Replace")
-          searchResults.items[i].font.color = "purple";
-          searchResults.items[i].font.highlightColor = "#FFFF00"; //Yellow
+    repPairs.push(eachTerm);
+  }
+  wl("(^_^＠) YUMMY!");
+  executeReplace(repPairs);
+}
+
+export async function readFromTextarea(ev) {
+  let reptext = document.getElementById("reptext").value.replace(/\r?\n/g, "\n");
+  const myAnnotator = new RegExp("^[0-9_/\[\\\]]+$");
+  const repPairs = [];
+  if (reptext == "") {
+    return;
+  }
+  const lines = reptext.split("\n");
+  for (let line of lines) {
+    if (line !== "") {
+      let eachTerm = line.split("\t");
+      if (eachTerm[0] !== "" && eachTerm[1] !== "") {
+        if (!myAnnotator.test(eachTerm[0])) {
+          repPairs.push(line.split("\t"));
         }
       }
-    await context.sync()
-    })
+    }
   }
+  wl("(^_^＠) YUMMY!");
+  executeReplace(repPairs);
+}
+
+export async function executeReplace(repPairs) {
+  let counter = 0;
+  return Word.run(async context => {
+    wlp("run run★")
+    for (let repPair of repPairs) {
+      counter++
+      wlp(counter)
+      // let searchResults = context.document.body.search(repPair[0], { matchCase: vc.matchCase, useWildcard: vc.useWildcard });
+      let searchResults = context.document.body.search(repPair[0]);
+      searchResults.load(["text", "font"]);
+      await context.sync()
+      for (var i = 0; i < searchResults.items.length; i++) {
+        searchResults.items[i].insertText("[_" + String(counter) + "_]" + repPair[1] + "[_/_]", "Replace")
+        searchResults.items[i].font.color = "purple";
+        searchResults.items[i].font.highlightColor = "#FFFF00"; //Yellow
+      }
+    }
+    await context.sync()
+  })
 }
 
 export async function run() {
